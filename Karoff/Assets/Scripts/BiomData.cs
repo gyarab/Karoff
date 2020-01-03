357aea6683c66b249d3d98b6b644038dadbf4501
tfshaman <tfshaman@gmail.com>
2020-01-03 18:29:10 +0100

    Added testing blank scriptable object

---
+++ Karoff/Assets/Scripts/BiomData.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New BiomData", menuName = "Biom Data", order = 51)]
+public class BiomData : ScriptableObject
+{
+    [SerializeField]
+    private string biomName;
+    [SerializeField]
+    private Material material;
+    [SerializeField]
+    private int status;
+    [SerializeField]
+    private int bonus;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
