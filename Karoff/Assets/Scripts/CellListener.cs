a36a1a8ae51357f02364bc832f746ee73a59759f
tfshaman <tfshaman@gmail.com>
2020-01-03 17:31:26 +0100

    Put click listener into methods

--- Karoff/Assets/Scripts/CellListener.cs
+++ Karoff/Assets/Scripts/CellListener.cs
@@ -6,6 +6,9 @@ public class CellListener : MonoBehaviour
 {
     public Material borderTile;
     Material select;
+    public Sprite s;
+
+    RaycastHit hitInfo = new RaycastHit();
 
     void Start()
     {
@@ -16,31 +19,53 @@ public class CellListener : MonoBehaviour
 
     void Update()
     {
-        mouseClickListener();
+        if (mouseClickListener())
+        {
+            action();
+        }
     }
 
-    protected void mouseClickListener() // Targets clicked object
+    protected bool mouseClickListener() // Targets clicked object
     {
         if (Input.GetMouseButtonDown(0))
         {
-            RaycastHit hitInfo = new RaycastHit();
             bool hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo);
             if (hit)
             {
-                if (hitInfo.transform.position.y != 0.75f)
-                { //checks if it's not border
-                    if (hitInfo.transform.position.y == 0.5f) // check if its not already used
-                    {
-                        select = hitInfo.transform.gameObject.GetComponent<MeshRenderer>().material;
-                    }
-                    if (hitInfo.transform.position.y == 0.25f) //uses
-                    {
-                        hitInfo.transform.position = new Vector3(hitInfo.transform.position.x, 0.5f, hitInfo.transform.position.z);
-                        hitInfo.transform.gameObject.GetComponent<MeshRenderer>().material = select;
-                    }
+                return hit;
+            }
+            else {
+                return false;
+            }
+
+        }
+        return false;
+    }
+
+    protected void action() {
+        if (hitInfo.transform.position.y != 0.75f)
+        { //checks if it's not border
+
+            if (hitInfo.transform.position.y == 0.5f) // check if its not already used
+            {
+                select = hitInfo.transform.gameObject.GetComponent<MeshRenderer>().material;
+            }
+            if (select != null)
+            {
+                if (hitInfo.transform.position.y == 0.25f) //uses
+                {
+                    hitInfo.transform.position = new Vector3(hitInfo.transform.position.x, 0.5f, hitInfo.transform.position.z);
+                    //hitInfo.transform.gameObject.GetComponent<MeshRenderer>().material = select;
+
+                    Debug.Log(select);
                 }
             }
         }
     }
+
 }
+//jentak btw sem davam odkazy z kterych se cerpalo pri tvorbe 
+//https://answers.unity.com/questions/411793/selecting-a-game-object-with-a-mouse-click-on-it.html
+//https://www.raywenderlich.com/2826197-scriptableobject-tutorial-getting-started#toc-anchor-003
+//https://docs.unity3d.com/Manual/index.html
 
