54b90048b0ae22d67bb040b17a7c65983b933770
tfshaman <tfshaman@gmail.com>
2020-02-01 19:36:53 +0100

    added buildings/buildings scriptable objects/first building desert testing/buildings set with boardsetup (some with disabled meshrenderer) and fixed set biome bug

---
+++ Karoff/Assets/Scripts/Building.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building : MonoBehaviour
+{
+    [SerializeField]
+    public BuildingData buildingData;
+
+    private void OnMouseDown()
+    {
+        //Debug.Log(biomData.BiomName); 
+        //Debug.Log(biomData.Material); 
+        //Debug.Log(biomData.Status);
+        ////Debug.Log("X is:" + biomData.X.ToString());
+        //Debug.Log("Y is:" + biomData.Y.ToString());
+        //Debug.Log("Z is:" + biomData.Z.ToString());
+    }
+
+}
