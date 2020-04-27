f366ec12109f6169c96bf5b93f0c9a037309104a
Darezar <darezar@eledris.com>
2020-04-27 15:27:23 +0200

    Changing Music

--- Karoff/Assets/Scripts/AudioManager/AudioManager.cs
+++ Karoff/Assets/Scripts/AudioManager/AudioManager.cs
@@ -53,4 +53,29 @@ public class AudioManager : MonoBehaviour
 		s.source.Play();
 	}
 
+    public void Stop(string sound)
+    {
+        Sound s = Array.Find(sounds, item => item.name == sound);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+        }
+
+        s.source.Stop();
+
+    }
+
+    public Sound Sound(string sound)
+    {
+        Sound s = Array.Find(sounds, item => item.name == sound);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return null;
+        }
+
+        return s;
+    }
+
 }
--- Karoff/Assets/Scripts/WinManager.cs
+++ Karoff/Assets/Scripts/WinManager.cs
@@ -18,6 +18,10 @@ public class WinManager : MonoBehaviour
     public GameObject winScreen;
     public TextMeshProUGUI winner;
 
+    public int music = 1;
+    public int music2Trigger = 10;
+    public int music3Trigger = 20;
+
     private void Awake()
     {
         rm = FindObjectOfType<ResourceManager>();
@@ -58,6 +62,48 @@ public class WinManager : MonoBehaviour
             //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
             return;
         }
+
+
+        if(music == 1 && (rm.blueResources[0] >= music2Trigger || rm.redResources[0] >= music2Trigger))
+        {
+            music = 2;
+            //FindObjectOfType<AudioManager>().Stop("Main1");
+            FindObjectOfType<AudioManager>().Play("Main2");
+            StartCoroutine(CrossFade(FindObjectOfType<AudioManager>().Sound("Main1"), FindObjectOfType<AudioManager>().Sound("Main2")));
+            return;
+        }
+
+        if(music == 2 && (rm.blueResources[0] >= music3Trigger || rm.redResources[0] >= music3Trigger))
+        {
+            music = 3;
+            //FindObjectOfType<AudioManager>().Stop("Main2");
+            FindObjectOfType<AudioManager>().Play("Main3");
+            StartCoroutine(CrossFade(FindObjectOfType<AudioManager>().Sound("Main2"), FindObjectOfType<AudioManager>().Sound("Main3")));
+            return;
+        }
+
+    }
+
+    public IEnumerator CrossFade(Sound end, Sound start)
+    {
+        bool done = false;
+        float startVol = start.source.volume;
+        start.source.volume = 0f;
+
+        while (!done)
+        {
+            end.source.volume -= Time.deltaTime;
+            start.source.volume += Time.deltaTime;
+
+            if(end.source.volume <= 0f && start.source.volume >= startVol)
+            {
+                done = true;
+            }
+
+            yield return new WaitForEndOfFrame();
+        }
+
+        end.source.Stop();
     }
 
     public void CheckTurns()
